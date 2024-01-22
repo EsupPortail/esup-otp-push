94daba6e0472cc27bb60483dc19ca9f850c8f3f2
Aymar Anli <Aymar.Anli@univ-paris1.fr>
2024-01-22 19:40:07 +0100

    feat: multi organizations, multi servers, multi accounts
    Can use the same app for multiple accounts

--- www/js/app.js
+++ www/js/app.js
@@ -16,22 +16,20 @@ var app = new Vue({
         mode : 'test',
         currentView: 'home',
         storage: undefined,
-        uid: undefined,
-        url: undefined,
         gcm_id: undefined,
         platform: undefined,
         manufacturer: undefined,
         model: undefined,
         totp: undefined,
         totpnb: undefined,
-        tokenSecret: undefined,
         additionalData: {
             text : undefined,
             action : undefined,
             lt : undefined,
-            totpKey : undefined,
-            totpName : undefined
+            totpKey : undefined
         },
+        otpServersObjects: {},
+        otpServersStack:[],
         push: undefined,
         notified : false,
         uid_input : undefined,
@@ -63,9 +61,8 @@ var app = new Vue({
                 StatusBar.backgroundColorByHexString("#212121");
             }
             this.storage= window.localStorage;
-            this.uid= this.storage.getItem('uid');
-            this.url= this.storage.getItem('url');
-            this.tokenSecret=this.storage.getItem('tokenSecret');
+            this.initOTPServers();
+            this.transfer2OtpServers();
             this.platform = device.platform;
             this.manufacturer = device.manufacturer;
             this.model = device.model
@@ -94,14 +91,16 @@ var app = new Vue({
                             self.gcm_id = data.registrationId;
                         }
                         else if (self.gcm_id != data.registrationId) {
-                                                        self.refresh(self.url, self.uid, self.tokenSecret, self.gcm_id, data.registrationId);
-                                                             }
-                                                             else {
-                                                             }
+                                for(otpServer in this.otpServersObjects){
+
+                                   self.refresh(otpServer.host, otpServer.uid, otpServer.tokenSecret, self.gcm_id, data.registrationId);
+                                }
+                            }
                     });
 
                     this.push.on('notification', function (data) {
                         self.additionalData = data.additionalData;
+                        self.additionalData.otpServer=data.additionalData.url+data.additionalData.uid
                         self.notification();
                     });
 
@@ -110,20 +109,38 @@ var app = new Vue({
                     });
                 },
         initAuth: function(){
+            this.otpServersStack=[];
+            for(otpServer in this.otpServersObjects)
+                this.otpServersStack.push(otpServer);
+
+            this.otpServerStatus(this.otpServersStack.pop());
+        },
+        otpServerStatus: function(otpServer){
             var self = this;
-            if(this.url!=null && this.uid!=null && this.tokenSecret!=null){
+            if(otpServer==null) return;
+            if(this.otpServersObjects[otpServer].host!=null && this.otpServersObjects[otpServer].uid!=null && this.otpServersObjects[otpServer].tokenSecret!=null){
                 $.ajax({
                     method : "GET",
-                    url: this.url + 'users/' + this.uid + '/methods/push/' + this.tokenSecret,
+                    url: this.otpServersObjects[otpServer].host + 'users/' + this.otpServersObjects[otpServer].uid + '/methods/push/' + this.otpServersObjects[otpServer].tokenSecret,
                     dataType: 'json',
                     cache: false,
                     success: function(data) {
                         if (data.code == "Ok") {
                             this.additionalData=data;
+                            this.additionalData.otpServer=otpServer;
                             self.notification();
-                            };
+                        }
+                        else{
+                            this.otpServerStatus(this.otpServersStack.pop());
+                        }
                     }.bind(this)
                 });
+            }
+        },
+        initOTPServers : function(){
+            var otpServers = localStorage.getItem('otpServers');
+            if (otpServers != null) {
+                this.otpServersObjects=JSON.parse(otpServers);
             }
         },
         scan: function () {
@@ -176,17 +193,18 @@ var app = new Vue({
                 cache: false,
                 success: function(data) {
                     if (data.code == "Ok") {
-                        this.uid = uid;
-                        this.storage.setItem('uid', uid);
-                        this.url = host;
-                        this.storage.setItem('url', host);
-                        this.tokenSecret = data.tokenSecret;
-                        this.storage.setItem('tokenSecret', data.tokenSecret);
+                        this.otpServersObjects[host+uid]={
+                            'host':host,
+                            'hostToken':data.hostToken,
+                            'tokenSecret':data.tokenSecret,
+                            'hostName':data.hostName,
+                            'uid':uid
+                        }
+                        this.storage.setItem('otpServers', JSON.stringify(this.otpServersObjects));
                         Materialize.toast("Synchronisation effectuée", 4000);
                         if(data.autoActivateTotp){
                             this.additionalData.totpKey=data.totpKey;
-                            this.additionalData.totpName=data.totpName;
-                            this.autoActivateTotp(host,uid);                            
+                            this.autoActivateTotp(host+uid);
                         }
                         else
                             this.navigate({target:{
@@ -208,15 +226,15 @@ var app = new Vue({
 
         },
 
-        autoActivateTotp: function(host,uid){
+        autoActivateTotp: function(otpServer){
             $.ajax({
                 method : "POST",
-                url: host+'users/'+ uid + '/methods/totp/autoActivateTotp/' + this.tokenSecret,
+                url: this.otpServersObjects[otpServer].host+'users/'+ this.otpServersObjects[otpServer].uid + '/methods/totp/autoActivateTotp/' + this.otpServersObjects[otpServer].tokenSecret,
                 dataType: 'json',
                 cache: false,
                 success: function(data) {
                     if (data.code == "Ok") {
-                        setAccount(this.additionalData.totpKey,this.additionalData.totpName);
+                        setAccount(this.additionalData.totpKey,this.getName(otpServer));
                         Materialize.toast("Activation TOTP effectuée", 4000);
                         this.navigate({target:{
                             name: 'home'
@@ -281,59 +299,67 @@ desactivateUser: function (url, uid, tokenSecret, gcm_id) {
                                                     });
 
                         },
-        desync: function () {
-         if (window.confirm("Voulez-vous vraiment désactiver la connexion avec votre mobile ?"))
-            this.user_desync();
+        desync: function (otpServer) {
+         if (window.confirm("Voulez-vous vraiment désactiver la connexion de "+ this.getName(otpServer)+" avec votre mobile ?"))
+            this.user_desync(otpServer);
         },
-    user_desync: function(){
+    user_desync: function(otpServer){
         var self = this;
             $.ajax({
                 method : "DELETE",
-                url: this.url + 'users/' + this.uid + '/methods/push/' + this.tokenSecret,
+                url: this.otpServersObjects[otpServer].host + 'users/' + this.otpServersObjects[otpServer].uid + '/methods/push/' + this.otpServersObjects[otpServer].tokenSecret,
                 dataType: 'json',
                 cache: false,
                 success: function(data) {
                     document.location.href = 'index.html';
                     Materialize.toast('Désactivation effectuée', 4000)
-                    self.uid = null;
-                    self.storage.removeItem('uid');
+                    delete this.otpServersObjects[otpServer];             
+                    self.storage.setItem('otpServers',JSON.stringify(this.otpServersObjects));
                     if(this.push!=null) this.push.clearAllNotifications();
                     self.checkTotp();
-                }.bind(this)
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    Materialize.toast(err.toString()+"Oups!! Probablement que le serveur n'est pas joignable",4000);
+                    }.bind(this)        
             });
     },
-
         notification: function () {
+            //recupération du token associé au host. on pourra supprimer ce code qd tout le monde aura migré
+            if(this.otpServersObjects[this.additionalData.otpServer].hostToken==null && this.additionalData.hostToken!=null){                
+                this.otpServersObjects[this.additionalData.otpServer].hostToken=this.additionalData.hostToken;
+                this.storage.setItem('otpServers',JSON.stringify(this.otpServersObjects));
+            }  
+            //MAJ libellé du serveur
+            if(this.otpServersObjects[this.additionalData.otpServer].hostName!=this.additionalData.hostName){                
+                this.otpServersObjects[this.additionalData.otpServer].hostName=this.additionalData.hostName;
+                this.storage.setItem('otpServers',JSON.stringify(this.otpServersObjects));
+            } 
             if (this.additionalData.action == 'auth'){
-		        if((this.url!=null && this.uid!=null && this.tokenSecret!=null)||this.additionalData.trustGcm_id) {
+                if(this.additionalData.otpServer!=null && this.otpServersObjects[this.additionalData.otpServer].host!=null && this.additionalData.hostToken==this.otpServersObjects[this.additionalData.otpServer].hostToken){
                 	this.notified = true;
+                    this.additionalData.text=this.additionalData.text.replace("compte","compte "+ this.getName(this.additionalData.otpServer)+" ");
 			        this.currentView = 'notify';
 		        }
    		        else {
 			        this.currentView = 'info';
 		        }
-            } else if (this.additionalData.action == "desync") {
-                this.user_desync();
+            } else if (this.additionalData.action == "desync" && this.additionalData.hostToken==this.otpServersObjects[this.additionalData.otpServer].hostToken) {
+                    this.user_desync(this.additionalData.otpServer);
+                    this.otpServerStatus(this.otpServersStack.pop());
             }
         },
 
         accept: function () {
-            if(this.tokenSecret==null) this.tokenSecret=self.gcm_id;
-            if(this.url==null) {this.url=this.additionalData.url;this.storage.setItem('url', this.url);}
-            if(this.uid==null) {this.uid=this.additionalData.uid;  this.storage.setItem('uid', this.uid);}
-
+            if(this.additionalData.hostToken==this.otpServersObjects[this.additionalData.otpServer].hostToken)
             $.ajax({
                 method : "POST",
-               url: this.url + 'users/' + this.uid + '/methods/push/' + this.additionalData.lt + '/' + this.tokenSecret,
+                url: this.otpServersObjects[this.additionalData.otpServer].host + 'users/' + this.otpServersObjects[this.additionalData.otpServer].uid + '/methods/push/' + this.additionalData.lt + '/' + this.otpServersObjects[this.additionalData.otpServer].tokenSecret,
                 dataType: 'json',
                 cache: false,
                 success: function(data) {
-                    if (data.code == "Ok" && data.tokenSecret!=null) {
-                        this.tokenSecret = data.tokenSecret;
-                        this.storage.setItem('tokenSecret', data.tokenSecret);
-                        };
                     this.notified = false;
                     this.additionalData = undefined;
+                    Materialize.toast("Authentification réussie!!!",4000);
                     if(this.push!=null) this.push.clearAllNotifications();
                     navigator.app.exitApp();
                 }.bind(this),
@@ -348,5 +374,30 @@ desactivateUser: function (url, uid, tokenSecret, gcm_id) {
             this.additionalData = undefined;
             navigator.app.exitApp();
         },
+        //on pourra supprimer ce code qd tout le monde aura migré
+        transfer2OtpServers: function(){
+            let uid= this.storage.getItem('uid');
+            let url= this.storage.getItem('url');
+            let tokenSecret=this.storage.getItem('tokenSecret');
+            if(uid!=null && url!=null && tokenSecret!=null){
+                this.otpServersObjects[url+uid]={
+                    "host":url,
+                    "uid":uid,
+                    "tokenSecret":tokenSecret
+                };
+                this.storage.setItem('otpServers',JSON.stringify(this.otpServersObjects));
+                this.storage.removeItem('uid');
+                this.storage.removeItem('url');
+                this.storage.removeItem('tokenSecret');
+            }
+        },
+        getName(otpServer){
+            if(this.otpServersObjects[otpServer].hostName==null || this.otpServersObjects[otpServer].hostName=="Esup Auth"){
+                let urlObj = new URL(this.otpServersObjects[otpServer].host);
+                return urlObj.hostname.split('.').slice(-2).join('.')+" ("+this.otpServersObjects[otpServer].uid+")";
+            }
+            else
+                return this.otpServersObjects[otpServer].hostName+" ("+this.otpServersObjects[otpServer].uid+")";
+        }
     }
 });
