109ba371e070531d009743b9a3167e059a638b25
MpenielG <Peniel-Gloriel.Mmen@univ-paris1.fr>
2025-05-30 12:10:56 +0200

    FIX: PUSH activation manuelle problème avec le nombre O

--- www/js/app.js
+++ www/js/app.js
@@ -375,7 +375,7 @@ var app = new Vue({
       if (this.host_input[this.host_input.length - 1] != "/") {
         this.host_input += "/";
       }
-      this.sync(this.host_input, this.uid_input.trim(), this.code_input);
+      this.sync(this.host_input, this.uid_input.trim(), parseInt(this.code_input));
     },
 
     sync: function (host, uid, code) {
