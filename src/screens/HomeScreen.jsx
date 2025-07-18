4ac2bb86ee06561c532033c46e94cffe2c03da4d
MpenielG <Peniel-Gloriel.Mmen@univ-paris1.fr>
2025-07-18 16:08:47 +0200

    UPDATE: HOME > NFC > NFC Non supporté

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,16 +1,25 @@
 import {useNavigation, useTheme} from '@react-navigation/native';
-import {StyleSheet, Text, View, FlatList, TouchableOpacity, Alert} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 import NfcScreen from './NfcScreen';
 import TotpScreen from './TotpScreen';
 import PushScreen from './PushScreen';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import CustomActionSheet from '../components/CustomActionSheet';
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
+import nfcManager from 'react-native-nfc-manager';
 
 export default function HomeScreen() {
   const {colors} = useTheme();
   const navigation = useNavigation();
   const [isActionSheetOpen, setIsActionSheetOpen] = useState(false);
+  const [isNfcSupported, setIsNfcSupported] = useState(null);
   const data = [];
 
   const handleScanQrCode = () => {
@@ -21,29 +30,44 @@ export default function HomeScreen() {
     navigation.navigate('ManualInput');
   };
 
+  const checkNfc = async () => {
+    const isSupported = await nfcManager.isSupported();
+    return isSupported;
+  };
+
+  useEffect(() => {
+    checkNfc().then(isSupported => {
+      setIsNfcSupported(isSupported);
+    });
+  }, []);
+
   return (
     <>
-    <FlatList
-      data={data}
-      renderItem={null}
-      keyExtractor={(_, index) => index.toString()}
-      ListHeaderComponent={
-        <>
-          <TotpScreen />
-          <View style={[styles.separator, {borderColor: 'grey'}]} />
-          <PushScreen />
-          <View style={[styles.separator, {borderColor: 'grey'}]} />
-          <NfcScreen />
-        </>
-      }
-      ListFooterComponent={
-        <View style={{height: 80}} />
-      }
-    />
-    <TouchableOpacity style={styles.floattingButton} onPress={() => setIsActionSheetOpen(true)}>
-      <Icon name="plus-circle" color={colors.primary} size={56} />
-    </TouchableOpacity>
-    <CustomActionSheet
+      <FlatList
+        data={data}
+        renderItem={null}
+        keyExtractor={(_, index) => index.toString()}
+        ListHeaderComponent={
+          <>
+            <TotpScreen />
+            <View style={[styles.separator, {borderColor: 'grey'}]} />
+            <PushScreen />
+            {isNfcSupported && (
+              <>
+                <View style={[styles.separator, {borderColor: 'grey'}]} />
+                <NfcScreen />
+              </>
+            )}
+          </>
+        }
+        ListFooterComponent={<View style={{height: 80}} />}
+      />
+      <TouchableOpacity
+        style={styles.floattingButton}
+        onPress={() => setIsActionSheetOpen(true)}>
+        <Icon name="plus-circle" color={colors.primary} size={56} />
+      </TouchableOpacity>
+      <CustomActionSheet
         visible={isActionSheetOpen}
         onClose={() => setIsActionSheetOpen(false)}
         actions={[
@@ -70,8 +94,8 @@ const styles = StyleSheet.create({
     elevation: 5,
     bottom: 5,
     right: 30,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
   },
