f607644efb3bd68c143a2a124b0b9f0f30ce80cb
MpenielG <Peniel-Gloriel.Mmen@univ-paris1.fr>
2025-09-08 13:22:55 +0200

    UPDATE: help doc > correction des coquilles

--- src/data/helpData.js
+++ src/data/helpData.js
@@ -2,12 +2,12 @@ export default [
     {
         title: 'Notification PUSH',
         key: 'push',
-        content: "L’authentification par Push mobile envoie une notification sur le smartphone de l’utilisateur lorsqu’une connexion est tentée. L’utilisateur doit alors approuver ou refuser la demande via l’application dédiée. Ce procédé ajoute une couche de sécurité en exigeant une validation explicite sur un appareil personnel. Pour activer cette méthode, scanner le QRCode affiché sur le portail de gestion l'authentification renforcée.",
+        content: "L’authentification par Push mobile envoie une notification sur le smartphone de l’utilisateur lorsqu’une connexion est tentée. L’utilisateur doit alors approuver ou refuser la demande via l’application dédiée. Ce procédé ajoute une couche de sécurité en exigeant une validation explicite sur un appareil personnel. Pour activer cette méthode, scanner le QRCode affiché sur le portail de gestion de l'authentification renforcée.",
     },
     {
         title: 'TOTP',
         key: 'totp',
-        content: "La méthode TOTP fournit un code de sécurité qui se renouvelle automatiquement toutes les 30 secondes. L’utilisateur doit saisir ce code en complément de son mot de passe afin de confirmer son identité. Ce mécanisme renforce la sécurité en rendant l’accès beaucoup plus difficile pour un tiers non autorisé. Pour activer cette méthode, scanner le QRCode affiché sur le portail de gestion l'authentification renforcée."
+        content: "La méthode TOTP fournit un code de sécurité qui se renouvelle automatiquement toutes les 30 secondes. L’utilisateur doit saisir ce code en complément de son mot de passe afin de confirmer son identité. Ce mécanisme renforce la sécurité en rendant l’accès beaucoup plus difficile pour un tiers non autorisé. Pour activer cette méthode, scanner le QRCode affiché sur le portail de gestion de l'authentification renforcée."
     },
     {
         title: 'NFC',
